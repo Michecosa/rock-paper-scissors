301a0c871d2eaf974b9763f12e5740c92d79eecb
Michecosa <micheladellagatta1@gmail.com>
2025-09-07 21:25:40 +0200

    Implement a function to associate a number with a move

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+let score = {
+  wins: 0,
+  loses: 0,
+  ties: 0
+};
+
+function pickComputerMove() {
+  const randomNumber = Math.random();
+
+  let computerMove = '';
+  
+  if(randomNumber >= 0 && randomNumber < 1/3) {
+    computerMove = 'rock';
+  } else if(randomNumber >= 1/3 && randomNumber < 2/3) {
+    computerMove = 'paper';
+  } else if(randomNumber >= 2/3 && randomNumber < 1) {
+    computerMove = 'scissors';
+  }
+
+  return computerMove;
+}
+
+function playGame(playerMove) {
+  const computerMove = pickComputerMove();
+}
